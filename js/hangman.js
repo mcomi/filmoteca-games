8eedbe8c83921acfa2be270d2992fde3edb566c3
Manuel Comi <mcomi@hotmail.com>
2020-05-13 19:31:53 -0500

    added hangman and puzzle

---
+++ js/hangman.js
@@ -0,0 +1,302 @@
+window.onload = function () {
+  var alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "ñ",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  var categories; // Array of topics
+  var chosenCategory; // Selected catagory
+  var getHint; // Word getHint
+  var word; // Formatted word
+  var wordSelected; // Selected word
+  var guess; // Geuss
+  var geusses = []; // Stored geusses
+  var lives; // Lives
+  var counter; // Count correct geusses
+  var space; // Number of spaces in word '-'
+
+  // Get elements
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+  var getHint = document.getElementById("hint");
+  var showClue = document.getElementById("clue");
+
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById("buttons");
+    letters = document.createElement("ul");
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = "alphabet";
+      list = document.createElement("li");
+      list.id = "letter";
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  };
+
+  // Select Catagory
+  var selectCat = function () {
+    if (chosenCategory === categories[0]) {
+      catagoryName.innerHTML = "Categoría películas mexicanas";
+    } else if (chosenCategory === categories[1]) {
+      catagoryName.innerHTML = "Categoría actores/actricez mexican@s";
+    } else if (chosenCategory === categories[2]) {
+      catagoryName.innerHTML = "Categoría directores/directoras mexican@s";
+    }
+  };
+
+  // Create geusses ul
+  result = function () {
+    wordHolder = document.getElementById("hold");
+    correct = document.createElement("ul");
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute("id", "my-word");
+      guess = document.createElement("li");
+      guess.setAttribute("class", "guess");
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space += 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  };
+
+  // Show lives
+  comments = function () {
+    showLives.innerHTML = "Tienes " + lives + " vidas";
+    if (lives < 1) {
+      showLives.innerHTML = "Juego terminado";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "¡Ganaste!";
+      }
+    }
+  };
+
+  // Animate man
+  var animate = function () {
+    var drawMe = lives;
+    drawArray[drawMe]();
+  };
+
+  // Hangman
+  canvas = function () {
+    myStickman = document.getElementById("stickman");
+    context = myStickman.getContext("2d");
+    context.beginPath();
+    context.strokeStyle = "#fff";
+    context.lineWidth = 2;
+  };
+
+  head = function () {
+    myStickman = document.getElementById("stickman");
+    context = myStickman.getContext("2d");
+    context.beginPath();
+    context.arc(60, 25, 10, 0, Math.PI * 2, true);
+    context.stroke();
+  };
+
+  draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke();
+  };
+
+  frame1 = function () {
+    draw(0, 150, 150, 150);
+  };
+
+  frame2 = function () {
+    draw(10, 0, 10, 600);
+  };
+
+  frame3 = function () {
+    draw(0, 5, 70, 5);
+  };
+
+  frame4 = function () {
+    draw(60, 5, 60, 15);
+  };
+
+  torso = function () {
+    draw(60, 36, 60, 70);
+  };
+
+  rightArm = function () {
+    draw(60, 46, 100, 50);
+  };
+
+  leftArm = function () {
+    draw(60, 46, 20, 50);
+  };
+
+  rightLeg = function () {
+    draw(60, 70, 100, 100);
+  };
+
+  leftLeg = function () {
+    draw(60, 70, 20, 100);
+  };
+
+  drawArray = [
+    rightLeg,
+    leftLeg,
+    rightArm,
+    leftArm,
+    torso,
+    head,
+    frame4,
+    frame3,
+    frame2,
+    frame1,
+  ];
+
+  // OnClick Function
+  check = function () {
+    list.onclick = function () {
+      var geuss = this.innerHTML;
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        }
+      }
+      var j = word.indexOf(geuss);
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    };
+  };
+
+  // Play
+  play = function () {
+    categories = [
+      [
+        "los olvidados",
+        "enamorada",
+        "nosotros los pobres",
+        "macario",
+        "el compadre mendoza",
+        "el tren fantasma",
+        "la mujer del puerto",
+      ],
+      [
+        "jorge negrete",
+        "pedro infante",
+        "maria felix",
+        "cantinflas",
+        "sara garcia",
+      ],
+      [
+        "luis buñuel",
+        "arturo ripstein",
+        "natalia beristain",
+        "guillermo del toro",
+        "alejandro gonzalez iñarritu",
+      ],
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    wordSelected = word;
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+    canvas();
+  };
+
+  play();
+
+  // Hint
+
+  getHint.onclick = function () {
+    hints = [
+      [
+        "Pista de los olvidados",
+        "Pista de enamorada",
+        "Pista de nosotros los pobres",
+        "Pista de macario",
+        "Pista de el compadre mendoza",
+        "Pista de el tren fantasma",
+        "Pista de la mujer del puerto",
+      ],
+      [
+        "pista de jorge negrete",
+        "pista de pedro infante",
+        "pista de julio aleman",
+        "pista de cantinflas",
+        "pista de german valdes",
+      ],
+      [
+        "pista de luis buñuel",
+        "pista de arturo ripstein",
+        "pista de jorge fons",
+        "pista de guillermo del toro",
+        "pista de alejandro gonzalez iñarritu",
+      ],
+    ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(wordSelected);
+    showClue.innerHTML = "Pista: - " + hints[catagoryIndex][hintIndex];
+  };
+
+  // Reset
+
+  document.getElementById("reset").onclick = function () {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  };
+};
